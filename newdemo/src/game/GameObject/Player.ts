5d0fb007771d2291c2581667f043d1dce9f83308
Christina0215 <53212713+Christina0215@users.noreply.github.com>
2019-08-24 21:25:36 +0800

    add player

--- newdemo/src/game/GameObject/Player.ts
+++ newdemo/src/game/GameObject/Player.ts
@@ -1,1 +1,23 @@
-class Player extends egret.Sprite {}
+class Player extends egret.DisplayObjectContainer {
+    public x: number;
+    public y: number;
+    public speedX: number;
+    public speedY: number;
+
+    constructor(x:number, y:number) {
+        super();
+        this.init();
+    }
+
+    private init(): void {
+        var sprite:egret.Sprite = new egret.Sprite();
+        sprite.graphics.beginFill(0xff0000);
+        sprite.graphics.drawCircle(0, 0, 50);
+        sprite.graphics.endFill();
+        this.addChild(sprite);
+    }
+
+    private move(speedX:number, speedY:number): void{
+        
+    }
+}
