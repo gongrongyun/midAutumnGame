5eb6462eed188f1cfc2ffefa0bb0061761c9749b
gongrongyun <1716175849@qq.com>
2019-08-26 09:46:37 +0800

    add move

--- newdemo/src/game/GameObject/Player.ts
+++ newdemo/src/game/GameObject/Player.ts
@@ -1,23 +1,81 @@
-class Player extends egret.DisplayObjectContainer {
+class Player extends egret.Sprite {
     public x: number;
     public y: number;
-    public speedX: number;
-    public speedY: number;
+    private speedX: number;
+    private speedY: number;
+    public accelerationX: number;
+    public accelerationY: number;
 
-    constructor(x:number, y:number) {
+    constructor(x: number, y: number) {
         super();
+        this.x = x;
+        this.y = y;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.accelerationX = 0;
+        this.accelerationY = 0;
         this.init();
     }
 
     private init(): void {
-        var sprite:egret.Sprite = new egret.Sprite();
-        sprite.graphics.beginFill(0xff0000);
-        sprite.graphics.drawCircle(0, 0, 50);
-        sprite.graphics.endFill();
-        this.addChild(sprite);
+        this.graphics.beginFill(0xff0000);
+        this.graphics.drawCircle(0, 0, 50);
+        this.graphics.endFill();
     }
 
-    private move(speedX:number, speedY:number): void{
-        
+    set SpeedX(speedX: number) {
+        this.speedX += speedX;
+        if (this.speedX > 50) {
+            this.speedX = 50;
+        }
+        if (this.speedX < -50) {
+            this.speedX = -50;
+        }
     }
+
+    set SpeedY(speedY: number) {
+        this.speedY += speedY;
+        if (this.speedY > 50) {
+            this.speedY = 50;
+        }
+        if (this.speedY < -50) {
+            this.speedY = -50;
+        }
+    }
+
+    get SpeedX(): number {
+        return this.speedX;
+    }
+
+    get SpeedY(): number {
+        return this.speedY;
+    }
+
+    // set AccelerationX(accelerationX: number) {
+    //     this.accelerationX += accelerationX;
+    //     if (this.accelerationX > 50) {
+    //         this.accelerationX = 50;
+    //     }
+    //     if (this.accelerationX < -50) {
+    //         this.accelerationX = -50;
+    //     }
+    // }
+
+    // set AccelerationY(accelerationY: number) {
+    //     this.accelerationY += accelerationY;
+    //     if (this.accelerationY > 50) {
+    //         this.accelerationY = 50;
+    //     }
+    //     if (this.accelerationY < -50) {
+    //         this.accelerationY = -50;
+    //     }
+    // }
+
+    // get AccelerationX(): number {
+    //     return this.accelerationX;
+    // }
+
+    // get AccelerationY(): number {
+    //     return this.accelerationY;
+    // }
 }
--- newdemo/src/game/Scene/GameScene.ts
+++ newdemo/src/game/Scene/GameScene.ts
@@ -1,21 +1,90 @@
 class GameScene extends egret.DisplayObjectContainer {
     private bgImg: egret.Sprite;
+    private motion: egret.DeviceOrientation;
+    private player: Player;
+    private beta: number;
+    private gamma: number;
+    private T: number;
+    private text: egret.TextField;
 
     constructor() {
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.addStage, this);
+        this.addEventListener(egret.Event.LEAVE_STAGE, egret.stopTick, this);
+        this.player = new Player(Param.StageWidth / 2, Param.StageHeight / 2);
+        this.bgImg = new egret.Sprite();
+        this.text = new egret.TextField();
+        this.beta = 0;
+        this.gamma = 0;
+        this.T = 0.016;
     }
 
     private addStage(): void {
-        this.SceneInit();
-        // this.addChild(new Player())
+        this.addBgImg();
+        this.addChild(this.player);
+        this.addChild(this.text);
+        this._run();
     }
 
-    private SceneInit(): void {
-        this.bgImg = new egret.Sprite();
+    private addBgImg(): void {
         this.bgImg.graphics.beginFill(0x000000, 1);
         this.bgImg.graphics.drawRect(0, 0, Param.StageWidth, Param.StageHeight);
         this.bgImg.graphics.endFill();
         this.addChild(this.bgImg);
+    }
+
+    private _run(): void {
+        this.motion = new egret.DeviceOrientation();
+        this.motion.addEventListener(egret.Event.CHANGE, this.onMotion, this);
+        this.motion.start();
+        egret.startTick(this.update, this);
+    }
+
+    private update(timeStamp: number): boolean {
+        this.player.x +=
+            this.player.SpeedX * this.T +
+            (1 / 2) * this.player.accelerationX * this.T * this.T;
+        this.player.y +=
+            this.player.SpeedY * this.T +
+            (1 / 2) * this.player.accelerationY * this.T * this.T;
+        if (this.player.x < 50) {
+            this.player.x = 50;
+        }
+        if (this.player.y < 50) {
+            this.player.y = 50;
+        }
+        if (this.player.y > 1086) {
+            this.player.y = 1086;
+        }
+        if (this.player.x > 590) {
+            this.player.x = 590;
+        }
+        this.player.SpeedX = this.player.accelerationX * this.T;
+        this.player.SpeedY = this.player.accelerationY * this.T;
+        this.text.text =
+            "x = " +
+            this.player.x +
+            "\n" +
+            "y = " +
+            this.player.y +
+            "\n" +
+            "speedx = " +
+            this.player.SpeedX +
+            "\n" +
+            "speedy = " +
+            this.player.SpeedY +
+            "\n" +
+            "accelerationx = " +
+            this.player.accelerationX +
+            "\n" +
+            "sccelerationy = " +
+            this.player.accelerationY +
+            "\n";
+        return true;
+    }
+
+    private onMotion(e: egret.OrientationEvent): void {
+        this.player.accelerationX = e.gamma - this.gamma;
+        this.player.accelerationY = e.beta - this.beta;
     }
 }
