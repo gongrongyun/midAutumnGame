fddb3056e86ddee89d2a9a6aa82941673c38df78
gongrongyun <1716175849@qq.com>
2019-09-01 14:36:54 +0800

    init

---
+++ mcDinner/Assets/Scripts/OpenManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class OpenManager : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = gameObject.GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += LoadNextScene;
+    }
+
+    // Update is called once per frame
+    void LoadNextScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ mcDinner/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Player player;
+    private Rigidbody2D rig;
+    private float acceleration;
+    private float horizontal;
+    private float vertical;
+    private float radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        acceleration = 1f;
+        rig = GetComponent<Rigidbody2D>();
+        radius = GetComponent<Collider2D>().bounds.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //horizontal = Input.acceleration.x;
+        //vertical = Input.acceleration.y;
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        rig.velocity = new Vector2(rig.velocity.x + horizontal * Time.fixedDeltaTime * acceleration, 
+            rig.velocity.y + vertical * Time.fixedDeltaTime * acceleration);
+        if (rig.position.x + radius > MainController.screenWidth / 2)
+        {
+            rig.position = new Vector2(MainController.screenWidth / 2 - radius, rig.position.y);
+            rig.velocity = new Vector2(-rig.velocity.x * 0.5f, rig.velocity.y);
+        }
+        if (rig.position.x - radius < -MainController.screenWidth / 2)
+        {
+            rig.position = new Vector2(-MainController.screenWidth / 2 + radius, rig.position.y);
+            rig.velocity = new Vector2(-rig.velocity.x * 0.5f, rig.velocity.y);
+        }
+        if (rig.position.y + radius > MainController.screenHeight / 2)
+        {
+            rig.position = new Vector2(rig.position.x, MainController.screenHeight / 2 - radius);
+            rig.velocity = new Vector2(rig.velocity.x, -rig.velocity.y * 0.5f);
+        }
+        if (rig.position.y - radius < -MainController.screenHeight / 2)
+        {
+            rig.position = new Vector2(rig.position.x, -MainController.screenHeight / 2 + radius);
+            rig.velocity = new Vector2(rig.velocity.x, -rig.velocity.y * 0.5f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("balabala");
+        if (collision.gameObject.tag == "MoonCake")
+        {
+            collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject, 1f);
+        }
+    }
+}
