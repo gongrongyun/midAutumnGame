de4cb2eafac06e32eb65e164b81cfa0236e8035e
Christina0215 <53212713+Christina0215@users.noreply.github.com>
2019-09-08 19:35:57 +0800

    ?

--- mcDinner/Assets/Scripts/ComboController.cs
+++ mcDinner/Assets/Scripts/ComboController.cs
@@ -14,13 +14,11 @@ public class ComboController : MonoBehaviour
         tempTime = 0;
 
 
-        //获取材质本来的属性  
         this.GetComponent<Renderer>().material.color = new Color
         (
                 this.GetComponent<Renderer>().material.color.r,
                 this.GetComponent<Renderer>().material.color.g,
-                this.GetComponent<Renderer>().material.color.b,
-                //需要改的就是这个属性：Alpha值  
+                this.GetComponent<Renderer>().material.color.b, 
                 this.GetComponent<Renderer>().material.color.a
         );
     }
@@ -37,12 +35,9 @@ public class ComboController : MonoBehaviour
                 this.GetComponent<Renderer>().material.color.r,
                 this.GetComponent<Renderer>().material.color.g,
                 this.GetComponent<Renderer>().material.color.b,
-
-
-            //减小Alpha值，从1-30秒逐渐淡化 ,数值越大淡化越慢 
-            gameObject.GetComponent<Renderer>().material.color.a - tempTime / 30 * Time.deltaTime
+                gameObject.GetComponent<Renderer>().material.color.a - tempTime / 2 * Time.deltaTime
             );
         }
-        Destroy(this.gameObject, 40.0f);//40秒后消除
+        Destroy(this.gameObject, 2.0f);//40秒后消除
     }
 }
